98742992fefaf37c4bdbf61b201367c70d1cb3dd
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2024-11-14 13:40:19 +0100

    Add untracked files

--- src/opgave01/CalculateVat.java
+++ src/opgave01/CalculateVat.java
@@ -1,7 +1,18 @@
 package opgave01;
+import java.util.Scanner;
+
 
 public class CalculateVat {
     public static void main(String[] args) {
         //Beskriv din algoritme
+        //Jeg vil lave
+        int moms;
+
+        Scanner input = new Scanner(System.in);
+
+        double beløb = input.nextDouble();
+
+        System.out.println(beløb);
+
     }
 }
